3cc858a1dcbdb346c3b73308d488d7e529113c51
pablogfz <116373669+pablogfz@users.noreply.github.com>
2022-12-06 09:00:13 +0100

    phping

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/assets/header.php';?>
-<?php include_once $_SERVER['DOCUMENT_ROOT'].'/assets/header.php';?>
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/assets/header.php';
+/* include_once $_SERVER['DOCUMENT_ROOT'].'/assets/header.php' */?>
 <?php echo $_SERVER['DOCUMENT_ROOT']; ?>
         <section id="section1">
             <h1 class="h1prueba1" style="color:blueviolet">Hola, soy Pablo <?php echo date("l/z"); ?></h1>
@@ -12,6 +13,8 @@
             <div class="intro"><img src="/imagenes/spanvsdiv.jpg" width="600" height="300" alt="este es el alt de la primera imagen subida para este ejercicio"></div>
             <span title="probando con titles para los span">y ahora vamos a probar a <div>poner un div dentro de un span</div></span>
     </section>
+    <?php echo 'HOLA CON PHP';?>
+    <?php echo "</p>HOLA CON PHP Y COMILLAS CON P";?>
     <section>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
         <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap</p>
@@ -24,6 +27,27 @@
         <img src="/imagenes/htmlcheatsheet.png" loading="lazy" width="600" height="800" title="título de chuleta html" alt="imagen muy grande sobre etiquetas html">
     </picture>
     </section>
+    <?php $variable1 = "</p>Variable 100";
+    echo $variable1;
+    ?>
+    <p>
+        <?php $variable1 = "Variable 1000 con php dentro de html";
+    echo $variable1;
+    ?> </p>
+     <?php $variable1 = "</p>Variable 100";
+    /*echo $variable1 da error;
+    */?>
+    <?php $variable1 = "</p>Variable 100";
+    echo "$variable1 no da error";
+    ?>
+        <?php $variable1 = "</p>Variable 100";
+    echo '$variable1 no da error con comilla simple';
+    ?>
+        <?php $variable1 = "</p>Variable 100";
+    echo "$variable1". " con punto no da error" . " que punto significa sumar" . "$variable1";
+    ?>
+        <?php $variable1 = "</p>Variable 100";
+    echo $variable2 ?>
     <div class="divcentro">esto es un div que quiero centrar</div>    
     <?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php';?>
     </html>
