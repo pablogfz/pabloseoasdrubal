fdc83e6fd9d6bd569b5240650fcaef224bcb3556
pablogfz <116373669+pablogfz@users.noreply.github.com>
2022-12-27 19:16:00 +0100

    [0]

--- paginanueva.php
+++ paginanueva.php
@@ -10,4 +10,24 @@ include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php';?>
 <p id="funciononclic">Función onclic</p>
 <button type="button" onclick='document.getElementById("funciononclic").innerHTML = "¡ESTÁ VIVO!"'>
 Botón pequeñito</button>
+<div id="selector1"></div>
+<script>
+document.getElementById("selector1").innerHTML = "¡PARDIEZ!"
+</script>
+<div class="selector5"></div>
+<script>
+document.getElementsByClassName("selector5")[0].innerHTML = "<h2>¡SAPRISTI!</h2>";
+</script>
+<div class="selector2"></div>
+<div class="selector2"></div>
+<h3>Seleccionado por una etiqueta con js</h3>
+<script>
+const collection = document.getElementsByClassName("selector2");
+for (let i = 0; i < collection.length; i++) {
+collection[i].innerHTML = "No entiendo esto del cero. Espera, a lo mejor lo que selecciona no es el nombre del elemento, sino su lugar";
+}
+</script>
+<script>
+document.getElementsByTagName("h3")[0].innerHTML = "<h2>Te convertí en h2, por listillo</h2>";
+</script>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php';?>
