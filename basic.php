86b58cf238bb6e66355b071f1110d826dadbff92
pablogfz <116373669+pablogfz@users.noreply.github.com>
2023-01-15 19:48:09 +0100

    chachachanges

--- basic.php
+++ basic.php
@@ -14,5 +14,5 @@ include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php';
 onclick="document.getElementById('eltiempo').innerHTML = Date()">
 ¿A qué día estamos?</button>
 </div>
-<p>Z5sa1dPnQ%@XDek7Gk esta es la contraseña del proyecto en Wordpress</p>
+<p>Z5sa1dPnQ%@XDek7Gk esta es la contraseña del proyecto en Wordpress que en el nuevo branch aparece con esta frase tan larga</p>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php';?>
