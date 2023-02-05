b714913ae99540601e13dd592daf524a7c62ef7a
pablogfz <116373669+pablogfz@users.noreply.github.com>
2023-02-05 17:38:51 +0100

    memetas

---
+++ unica.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
+<title>
+Título de Metaetiquetas 1, que se mide en píxeles (580 max)
+</title>
+<meta name="description" content="Que no has cerrado el footer, acho">
+<meta name="robots" content="index, follow, max-snippet:25, noimageindex, unavailable_after:[2033-09-27]">
+<!--<meta name="robots" content="noarchive"> No almacena la información en caché-->
+<!--<meta name="robots" content="nositelinkssearchbox"> No aparece buscador (ej justeat)-->
+<!--<meta name="robots" content="nosnippet o max-snippet:0"> No aparece descripción. Si pusiéramos max-snippet:-1 significaría que no hay límite de caracteres-->
+<!--<meta name="robots" content="notranslante"> El contenido no es traducible-->
+</head>
+<body>
+<h1>Metaetiquetas 1</h1>
+<div> 
+Crea un meta robots que solo afecte a Google quee:
+<ul>
+    <li>Sea indexable.</li>
+    <li>Se puedan seguir los enlaces de la página.</li>
+    <li>No permita más de 25 caracteres en la meta description.</li>
+    <li>No se puedan indexar las imágenes.</li>
+    <li>El contenido deje de estar indexable el 27/09/2033</li>
+</ul>
+</div> 
+<footer>
+</footer>
+</body>
