07545de0900ba7ecd2bcad04eda5ac8e728c0261
pablogfz <116373669+pablogfz@users.noreply.github.com>
2023-02-12 00:01:01 +0100

    metas1

--- unica.php
+++ unica.php
@@ -6,19 +6,27 @@
 <title>
 Título de Metaetiquetas 1, que se mide en píxeles (580 max)
 </title>
-<meta name="description" content="Que no has cerrado el footer, acho">
+<meta name="description" content="Que no has cerrado el footer, acho.">
 <meta name="robots" content="index, follow, max-snippet:25, noimageindex, unavailable_after:[2033-09-27]">
 <!--<meta name="robots" content="noarchive"> No almacena la información en caché-->
-<!--<meta name="robots" content="nositelinkssearchbox"> No aparece buscador (ej justeat)-->
+<!--<meta name="robots" content="nositelinkssearchbox"> No aparece buscador. Cuando los usuarios buscan tu sitio, 
+a veces en los resultados de la Búsqueda de Google aparece un cuadro de búsqueda específico de tu sitio junto 
+con enlaces directos a algunas de tus páginas-->
 <!--<meta name="robots" content="nosnippet o max-snippet:0"> No aparece descripción. Si pusiéramos max-snippet:-1 significaría que no hay límite de caracteres-->
 <!--<meta name="robots" content="notranslante"> El contenido no es traducible-->
 <meta http-equiv="refresh" content="15; url=https://www.google.es/"/>
 <meta http-equiv="refresh" content="3" /> 
+<meta property="og:type" content="website">
+<!--<meta property="og:url" content="Tipo canonical">
+<meta property="og:title" content="Pues la title será">
+<meta property="og:description" content="Es como la meta description">
+<meta property="og:image:secure_url" content="Emplea una imagen con https">
+<meta property="og:image" content="Una imagen que represente cosas">-->
 </head>
 <body>
 <h1>Metaetiquetas 1</h1>
-<div> 
-Crea un meta robots que solo afecte a Google quee:
+<div data-nosnippet> 
+Data-nosnippet permite que el contenido de la etiqueta no aparezca en la SERP. Se pone en las etiquetas div, span y section
 <ul>
     <li>Sea indexable.</li>
     <li>Se puedan seguir los enlaces de la página.</li>
