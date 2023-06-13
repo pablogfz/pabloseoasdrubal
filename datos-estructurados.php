0ec7ab0f6609f3df9537a16b60280892d8192bf9
pablogfz <116373669+pablogfz@users.noreply.github.com>
2023-06-13 16:54:29 +0200

    datosestructurados

---
+++ datos-estructurados.php
@@ -0,0 +1,101 @@
+<script type="application/ld+json">
+  {
+    "@context": "https://schema.org",
+    "@type": "Product",
+    "sku": "trinket-12345",
+    "name": "Bote de testosterona en polvo",
+    "description": "Bote de testosterona en polvo de origen osil",
+    "image": [
+        "https://example.com/photos/16x9/trinket.jpg",
+        "https://example.com/photos/4x3/trinket.jpg",
+        "https://example.com/photos/1x1/trinket.jpg"
+      ],
+    "brand": {
+      "@type": "Brand",
+      "name": "Powerbear"
+    },
+    "offers": {
+      "@type": "Offer",
+      "url": "https://www.powerbear.com/testosterona/",
+      "priceCurrency": "EUR",
+      "price": "25",
+      "availability": "https://schema.org/InStock",
+      "aggregateRating": {
+          "@type": "itemReviewed",
+        "name": "testobear",
+          "ratingValue": 4.4,
+          "reviewCount": 89
+        }
+    }
+  }
+</script>
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Article",
+  "headline": "Lo buena que es la testosterona Powerbear",
+  "image": [
+    "https://powerbear.com/photos/1x1/photo.jpg",
+    "https://powerbear.com/photos/4x3/photo.jpg",
+    "https://powerbear.com/photos/16x9/photo.jpg"
+  ],
+  "datePublished": "2021-12-25T00:00:00+00:00",
+  "dateModified": "2022-02-28T00:00:00+00:00",
+  "author": {
+    "@type": "Person",
+    "name": "John Grizzlie",
+    "url": "https://powerbear.com/profile/johngrizzlie"
+  },
+  "publisher": {
+    "@type": "Organization",
+    "name": "Powerbear",
+    "url": "https://www.powerbear.com"
+  },
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "url": "https://www.powerbear.com/beneficios-testosterona-powerbear"
+  }
+}
+</script>
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Store",
+  "image": [
+    "https://powerbear.com/photos/1x1/photo.jpg",
+    "https://powerbear.com/photos/4x3/photo.jpg",
+    "https://powerbear.com/photos/16x9/photo.jpg"
+  ],
+  "name": "Powerbear",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Rúa das Rosas, 123",
+    "addressLocality": "Vilanovela",
+    "addressRegion": "A Coruña",
+    "postalCode": "36987",
+    "addressCountry": "Galicia"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 40.123456,
+    "longitude": -74.123456
+  },
+  "url": "https://www.powerbear.com",
+  "telephone": "+123456789",
+  "priceRange": "€",
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      "opens": "10:00",
+      "closes": "16:30"
+    }
+  ],
+  "paymentAccepted": "Cash, Credit Card",
+  "currenciesAccepted": "EUR",
+  "hasMap": "https://www.example.com/map",
+  "description": "Powerbear es una compañía de venta al por menor de suplementos testorenosos de origen osil"
+}
+</script>
